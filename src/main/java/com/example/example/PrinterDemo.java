07b00427f8945e3df1ebbf1989280a4dd7287420
Humberto García Arellano <humberbrawl@gmail.com>
2023-10-28 15:23:35 +0200

    printer center static data

--- src/main/java/com/example/example/PrinterDemo.java
+++ src/main/java/com/example/example/PrinterDemo.java
@@ -6,6 +6,7 @@ import java.awt.print.Paper;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,13 +30,14 @@ public class PrinterDemo {
         String address = "Cl Juan Bautista Vives, 1";
         String phone = "747857939";
 
+        int contentWidth = 226;
         int staticContentHeight = 6 * 12;
         int dynamicContentHeight = items.size() * 12;
         int totalHeight = staticContentHeight + dynamicContentHeight;
 
         Paper paper = new Paper();
-        paper.setSize(226.77, totalHeight);
-        paper.setImageableArea(0, 0, 226.77, totalHeight);
+        paper.setSize(contentWidth, totalHeight);
+        paper.setImageableArea(0, 0, contentWidth, totalHeight);
 
         pageFormat.setPaper(paper);
 
@@ -52,28 +54,38 @@ public class PrinterDemo {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
                 Font smallFont = new Font("Arial", Font.PLAIN, 12);
                 Font headerFont = new Font("Arial", Font.BOLD, 14);
+                FontMetrics fontMetrics = g.getFontMetrics(headerFont);
+
+                int headerCompanyName = (contentWidth - fontMetrics.stringWidth(companyName)) / 2;
+                int headerCif = (contentWidth - fontMetrics.stringWidth(cif)) / 2;
+                int headerAddress = (contentWidth - fontMetrics.stringWidth(address)) / 2;
+                int headerPhone = (contentWidth - fontMetrics.stringWidth(phone)) / 2;
+                int headerDate = (contentWidth - fontMetrics.stringWidth(dateFormat.format(date))) / 2;
                 g.setFont(headerFont);
 
-                g.drawString(companyName, x, y);
+                g.drawString(companyName, headerCompanyName, y);
                 y += 2 * lineHeight;
 
-                g.drawLine(0, y - 6, 226, y - 6);
-                y += 2 * lineHeight;
-
+                fontMetrics = g.getFontMetrics(smallFont);
                 g.setFont(smallFont);
-                g.drawString(cif, x, y);
-                y += lineHeight;
-                g.drawString(address, x, y);
-                y += lineHeight;
-                g.drawString(phone, x, y);
+
+                g.drawString(cif, headerCif, y);
                 y += lineHeight;
 
-                g.drawString(dateFormat.format(date), x, y);
-                y += lineHeight;
+                int dynamicContentY = y;
+
+                g.drawString(address, headerAddress, dynamicContentY);
+                dynamicContentY += lineHeight;
+
+                g.drawString(phone, headerPhone, dynamicContentY);
+                dynamicContentY += lineHeight;
+
+                g.drawString(dateFormat.format(date), headerDate, dynamicContentY);
+                dynamicContentY += lineHeight;
 
                 for (String item : items) {
-                    g.drawString(item, x, y);
-                    y += lineHeight;
+                    g.drawString(item, x, dynamicContentY);
+                    dynamicContentY += lineHeight;
                 }
 
                 return Printable.PAGE_EXISTS;
